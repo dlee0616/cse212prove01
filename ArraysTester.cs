bd789ad50607f5d2af47282878400115b68172c3
dlee0616 <98242430+dlee0616@users.noreply.github.com>
2023-01-19 11:22:30 -0800

    d

---
+++ ArraysTester.cs
@@ -0,0 +1,67 @@
+namespace prove_01;
+
+public static class ArraysTester {
+    /// <summary>
+    /// Entry point for the Prove 1 tests
+    /// </summary>
+    public static void Run() {
+        // Sample Test Cases (may not be comprehensive)
+        Console.WriteLine("\n=========== PROBLEM 1 TESTS ===========");
+        double[] multiples = MultiplesOf(7, 5);
+        Console.WriteLine($"<double>{{{string.Join(',', multiples)}}}"); // <double>{7, 14, 21, 28, 35}
+        multiples = MultiplesOf(1.5, 10);
+        Console.WriteLine($"<double>{{{string.Join(',', multiples)}}}"); // <double>{1.5, 3.0, 4.5, 6.0, 7.5, 9.0, 10.5, 12.0, 13.5, 15.0}
+        multiples = MultiplesOf(-2, 10);
+        Console.WriteLine($"<double>{{{string.Join(',', multiples)}}}"); // <double>{-2, -4, -6, -8, -10, -12, -14, -16, -18, -20}
+
+        Console.WriteLine("\n=========== PROBLEM 2 TESTS ===========");
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        RotateListRight(numbers, 1);
+        Console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{9, 1, 2, 3, 4, 5, 6, 7, 8} numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };z∆ RotateListRight(numbers, 5);
+        Console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{5, 6, 7, 8, 9, 1, 2, 3, 4}
+        numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        RotateListRight(numbers, 3);
+        Console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{7, 8, 9, 1, 2, 3, 4, 5, 6}
+        numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        RotateListRight(numbers, 9);
+        Console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{1, 2, 3, 4, 5, 6, 7, 8, 9}
+    }
+    /// <summary>
+    /// This function will produce a list of size 'length' starting with 'number' followed by multiples of 'number'.  For 
+    /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
+    /// integer greater than 0.
+    /// </summary>
+    /// <returns>array of doubles that are the multiples of the supplied number</returns>
+    private static double[] MultiplesOf(double number, int length)
+    {
+        // TODO Problem 1 Start
+        //make a new list and pass in length 
+        double[] multiples = new double[length];
+        //make a for loop to access number item in list
+        for(int i = 1;i < length + 1 ;i++)
+        {
+            //get multiples
+            double result = number * i;
+            multiples[i-1] = result;
+        }
+        //return list
+        return multiples;
+    }
+    
+    /// <summary>
+    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
+    /// &lt;List&gt;{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 5 then the list returned should be 
+    /// &lt;List&gt;{5, 6, 7, 8, 9, 1, 2, 3, 4}.  The value of amount will be in the range of 1 and 
+    /// <code>data.Count</code>.
+    /// </summary>
+    private static void RotateListRight(List<int> data, int amount)
+    {
+        // TODO Problem 2 Start
+        //make sublist from amount integer to the last ineteger of the list..
+        var subList = data.GetRange(data.Count - amount, amount);
+        //remove sublist from original list 
+        data.RemoveRange(data.Count - amount, amount);
+        //insert subList to the begninning of original list 
+        data.InsertRange(0, subList);
+    }
+}
